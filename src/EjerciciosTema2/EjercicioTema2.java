145cbeec10d093628a747e444ac7d39ae16466a8
Alex <alexbastidas.dev@gmail.com>
2022-07-23 16:40:09 -0500

    ejercicio tema 2

--- src/EjerciciosTema2/EjercicioTema2.java
+++ src/EjerciciosTema2/EjercicioTema2.java
@@ -1,2 +1,15 @@
-package EjerciciosTema2;public class EjercicioTema2 {
+package EjerciciosTema2;
+
+public class EjercicioTema2 {
+    public static void main(String[] args) {
+        double precioConIva = precioConIva(200.0);
+        System.out.println("El precio con iva es: " + precioConIva);
+    }
+
+    static double precioConIva(double precio) {
+        double iva = precio * 0.19;
+        double precioConIva = precio + iva;
+        return precioConIva;
+    }
+
 }
