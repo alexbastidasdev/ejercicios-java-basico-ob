8356c5b4b7f6cd39097c060b69e06e56dc8a3ad8
Alex <alexbastidas.dev@gmail.com>
2022-07-22 21:32:32 -0500

    ejercicio 1

---
+++ src/EjerciciosTema1/Ejercicio1.java
@@ -0,0 +1,32 @@
+package EjerciciosTema1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // enteros
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+
+        System.out.println("Numeros enteros: " + number1 + " , " + number2 + " , " + number3 + " , " + number4);
+
+        // decimales
+        float decimal1 = 1.1f;
+        double decimal2 = 2.2;
+
+        System.out.println("Decimales: " + decimal1 + " , " + decimal2);
+
+        // cadena de texto
+        String nombre = "Juan";
+        String apellido = "Perez";
+
+        // caracter
+        char caracter1 = 'a';
+
+        System.out.println("Cadena de texto: " + nombre + " " + apellido + "\nCaracter: " + caracter1);
+
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+    }
+}
