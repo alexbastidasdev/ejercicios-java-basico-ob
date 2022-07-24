fbadfa06188bd6ed5c9cf8c1c8aaeb4794fc8af1
Alex <alexbastidas.dev@gmail.com>
2022-07-23 19:48:48 -0500

    Ejercicio 3

--- src/EjerciciosTema3/EjercicioTema3.java
+++ src/EjerciciosTema3/EjercicioTema3.java
@@ -1,2 +1,16 @@
-package EjerciciosTema3;public class EjercicioTema3 {
+package EjerciciosTema3;
+
+public class EjercicioTema3 {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Juan", "Pedro", "Maria", "Pablo"};
+        String texto = "";
+        for (int i = 0; i < nombres.length; i++) {
+            texto += nombres[i] + ", ";
+            if (i == nombres.length - 1) {
+                texto += nombres[i];
+            }
+        }
+        System.out.println(texto);
+    }
 }
